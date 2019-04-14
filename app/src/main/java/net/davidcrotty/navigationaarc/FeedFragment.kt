5a41832c97649889a79c4d1d9234bc4f951254f2
David Crotty <david.crotty.work@gmail.com>
2019-04-14 17:53:23 +0100

    wip: incomplete nav

--- app/src/main/java/net/davidcrotty/navigationaarc/FeedFragment.kt
+++ app/src/main/java/net/davidcrotty/navigationaarc/FeedFragment.kt
@@ -5,10 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.fragment_feed.*
 
 class FeedFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return layoutInflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        upload_button.setOnClickListener {
+            NavHostFragment.findNavController(this).navigate(R.id.feed_action)
+        }
     }
 }
--- app/src/main/java/net/davidcrotty/navigationaarc/MainActivity.kt
+++ app/src/main/java/net/davidcrotty/navigationaarc/MainActivity.kt
@@ -3,6 +3,15 @@ package net.davidcrotty.navigationaarc
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * Questions: How do we animate between fragments?
+ *
+ * Can we synthesize a navigation graph programmatically? ie: a flow changes
+ *
+ * Can we animate a view out of the screen?
+ *
+ * Can we instantiate fragment variants? ie, I want another fragment instead of the same instance?
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
